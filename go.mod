7d3b49bd9211a5635bc06cc736804d9dbf74fbf1
Jason Tarasovic <jason@tarasovic.com>
2020-06-07 12:31:39 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jtarasovic/catester
+
+go 1.14
+
+require github.com/go-acme/lego/v3 v3.7.0
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"io/ioutil"
+	"log"
+
+	"github.com/go-acme/lego/v3/lego"
+	"github.com/go-acme/lego/v3/registration"
+)
+
+// User implements the registration.User interface
+type User struct {
+	Email        string
+	Registration *registration.Resource
+	Key          crypto.PrivateKey
+}
+
+func (u *User) GetEmail() string {
+	return u.Email
+}
+
+func (u User) GetRegistration() *registration.Resource {
+	return u.Registration
+}
+
+func (u *User) GetPrivateKey() crypto.PrivateKey {
+	return u.Key
+}
+
+func main() {
+	// TODO(jdt): use a flag or something not hardcoded
+	key, err := ioutil.ReadFile("./key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := parseRSAPrivateKeyFromPEM(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := User{
+		Email: "",
+		Key:   privateKey,
+	}
+
+	client, err := lego.NewClient(lego.NewConfig(&user))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reg, err := client.Registration.ResolveAccountByKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	user.Registration = reg
+
+	log.Printf("%+v\n", reg)
+}
+
+func parseRSAPrivateKeyFromPEM(privPEM []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(privPEM)
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
